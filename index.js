8089f7ed599a2e335d6d01a4caefc3a5d913d527
WaldoJeffers <achille.urbain@gmail.com>
2017-12-14 16:44:10 +0100

    Add es5 build (#1)
    * Add es5 build
    
    * 0.1.12

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
-import {
-  NativeModules,
-} from 'react-native';
+import { NativeModules } from 'react-native';
 
-const {SegmentAnalytics} = NativeModules;
+const { SegmentAnalytics } = NativeModules;
 
 export default {
-  setup: function (configKey: string) {
+  setup: function(configKey) {
     SegmentAnalytics.setup(configKey);
   },
 
-  identify: function (userId: string, traits: Object) {
+  identify: function(userId, traits) {
     SegmentAnalytics.identify(userId, traits);
   },
 
-  track: function (trackText: string, properties: Object) {
+  track: function(trackText, properties) {
     SegmentAnalytics.track(trackText, properties);
   },
 
-  screen: function (screenName: string, properties: Object) {
+  screen: function(screenName, properties) {
     SegmentAnalytics.screen(screenName, properties);
   },
 
-  alias: function (newId: string) {
+  alias: function(newId) {
     SegmentAnalytics.alias(newId);
   },
 
   reset: function() {
     SegmentAnalytics.reset();
-  }
+  },
 };
